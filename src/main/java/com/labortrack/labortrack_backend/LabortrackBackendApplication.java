78d886ca68d6f9613bf55f17e04af8441d5cc3a9
Yonelvyn Morel <ymorel98@glhs.nyc>
2026-07-02 15:01:43 -0400

    Initial Spring Boot app and PostgreSQL configuration setup

---
+++ src/main/java/com/labortrack/labortrack_backend/LabortrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.labortrack.labortrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabortrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabortrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labortrack/labortrack_backend/LabortrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labortrack.labortrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabortrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
