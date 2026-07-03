b2f31b187bd9c31b4f919d81ee5b6942a812fbff
Yonelvyn Morel <ymorel98@glhs.nyc>
2026-07-02 20:50:59 -0400

    Implement the User entity class with fields, constraints and data types. Use V1 as the reference. Also include on create and onUpdate, vars change

--- src/main/java/com/labortrack/labortrack_backend/user/entity/User.java
+++ src/main/java/com/labortrack/labortrack_backend/user/entity/User.java
@@ -1,4 +1,57 @@
 package com.labortrack.labortrack_backend.user.entity;
 
+import com.labortrack.labortrack_backend.user.enums.UserRole;
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "company_id", nullable = false)
+    private Long companyId;
+
+    @Column(nullable = false, unique = true, columnDefinition="citext")
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private UserRole role;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @Column(name = "must_change_password", nullable = false)
+    private boolean mustChangePassword = true;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    // HELPER METHODS
+    @PrePersist
+    protected void onCreate() {
+        OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        if (this.createdAt == null) {
+            this.createdAt = now;
+        }
+        if (this.updatedAt == null) {
+            this.updatedAt = now;
+        }
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = OffsetDateTime.now(ZoneOffset.UTC);
+    }
 }
