7a799e3f088db9d29533241f36b87ece3196dc1d
Yonelvyn Morel <ymorel98@glhs.nyc>
2026-07-03 22:26:19 -0400

    Map JPA entity relationships, app compile correctly

--- src/main/java/com/labortrack/labortrack_backend/company/entity/Company.java
+++ src/main/java/com/labortrack/labortrack_backend/company/entity/Company.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="companies")
+@Table(name = "companies")
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
