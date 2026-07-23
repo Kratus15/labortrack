f656d3084a2e42d3a04318caea15051e8386c790
Yonelvyn Morel <ymorel98@glhs.nyc>
2026-07-23 10:49:16 -0400

    Added password encoder configuration

---
+++ src/main/java/com/labortrack/labortrack_backend/common/config/PasswordConfig.java
@@ -0,0 +1,19 @@
+package com.labortrack.labortrack_backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Convert normal plain password into encrypted more secure ones, making it
+ * less likely to be hack or be vulnerable.
+ */
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
